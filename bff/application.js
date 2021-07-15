c1b9970ab260fc1dc39aa0274031bff1c7320986
Bruno Jutkoski <brunojutkoski@ciandt.com>
2021-07-15 12:08:05 -0300

    mustache partials

--- bff/application.js
+++ bff/application.js
@@ -5,10 +5,32 @@ const app = express();
 const port = 3000;
 
 
-
 app.get('/', (req, res) => {
-    const template = fs.readFileSync(`${__dirname}/views/dojo.mustache`, 'utf-8');
-    res.send(mustache.render(template));
+    const template = fs.readFileSync(`${__dirname}/views/base.mustache`, 'utf-8');
+    const header = fs.readFileSync(`${__dirname}/views/header.mustache`, 'utf-8');
+    const content = fs.readFileSync(`${__dirname}/views/home.mustache`, 'utf-8');
+    const footer = fs.readFileSync(`${__dirname}/views/footer.mustache`, 'utf-8');
+    const data = { name: 'Bruno', isPolite: false };
+    const partials = {
+      header,
+      content,
+      footer,
+    }
+    res.send(mustache.render(template, data, partials));
+})
+
+app.get('/contact', (req, res) => {
+  const template = fs.readFileSync(`${__dirname}/views/base.mustache`, 'utf-8');
+  const header = fs.readFileSync(`${__dirname}/views/header.mustache`, 'utf-8');
+  const content = fs.readFileSync(`${__dirname}/views/contact.mustache`, 'utf-8');
+  const footer = fs.readFileSync(`${__dirname}/views/footer.mustache`, 'utf-8');
+  const data = { name: 'Bruno', isPolite: false };
+  const partials = {
+    header,
+    content,
+    footer,
+  }
+  res.send(mustache.render(template, data, partials));
 })
 
 app.listen(port, () => {
